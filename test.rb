5ea62149b4af5c298cfaae57c1bcfd8b6b01f4bd
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-08-23 22:20:49 +0300

    refactoring

--- test.rb
+++ test.rb
@@ -4,13 +4,16 @@ require 'capybara/dsl'
 Capybara.current_driver = :selenium
 
 class Page
+
   include Capybara::DSL
+
   LOCATOR = '.friends_field>a>b'
+
   def login
-  	visit 'https://vk.com/login'
-  	fill_in 'email', with: 'example@example.com'
-	  fill_in 'pass', with: 'password'
-	  click_button 'Log in'
+    visit 'https://vk.com/login'
+    fill_in 'email', with: 'example@example.com'
+    fill_in 'pass', with: 'password'
+    click_button 'Log in'
     raise "Authorization has been failed" unless has_link?('log out')
     puts "Authorization is successful"
   end
@@ -21,14 +24,6 @@ class Page
   end
 
   def scroll_down
-    count = nil
-    while count != all(LOCATOR).count do
-      count = all(LOCATOR).count
-      page.driver.browser.find_element(:link, "about").location_once_scrolled_into_view
-    end
-  end
-
-  def scroll_down2
     count_of_friends_online = find("#friends_summary").text.match(/\d+/)[0].to_i
     count_of_scrooll = (count_of_friends_online + 1)/15
     count_of_scrooll.times {page.driver.browser.find_element(:link, "about").location_once_scrolled_into_view}
@@ -36,10 +31,10 @@ class Page
 
   def display_online_friends
     within(".main_panel") do
-    	puts "#{all(LOCATOR).count} friend(-s) online"
+      puts "#{all(LOCATOR).count} friend(-s) online"
       all(LOCATOR).each_with_index do |friend, index|
         index += 1
-      	puts "#{index.to_s}) #{friend.text}"
+        puts "#{index.to_s}) #{friend.text}"
       end
     end
   end
@@ -48,6 +43,6 @@ end
 page = Page.new
 page.login
 page.go_to_online_friends
-page.scroll_down2
+page.scroll_down
 page.display_online_friends
 puts "Task finish"
