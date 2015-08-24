fed024ddd3478e0d118fa4ac56e5814973fc9e39
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-08-24 16:58:40 +0300

    add test rspec for display online friends and refactoring

--- test_spec.rb
+++ test_spec.rb
@@ -1,36 +1,41 @@
 require 'rspec'
 require './page.rb'
 
-describe "test" do
+describe "page class" do
   subject { Page.new }
 
-  describe "Page class" do
-    it { expect(subject).to be_a Page }
-    it { should respond_to(:login, :go_to_online_friends, :scroll_down_if_need, :display_online_friends) }
-  end
+  it { expect(subject).to be_a Page }
+  it { expect(subject).to respond_to(:login, :go_to_online_friends, :scroll_down_if_need, :display_online_friends) }
 
   describe '#login' do
-    context 'Failed authorization' do
+    context 'failed authorization' do
       it { expect { subject.login('example@example.com', 'password') }.to raise_error 'Authorization has been failed'}
     end
 
     # to pass this tests fill login with valid data
-    context 'Success authorization' do
+    context 'success authorization' do
       it { expect(subject.login('email', 'password')).to eq('Authorization is successful') }
 
-      describe 'Visit online friends' do
-        it do
-          subject.go_to_online_friends
-          should have_title('Friends Online')
+      describe '#go_to_online_friends' do
+        before { subject.go_to_online_friends }
+
+        it { should have_title('Friends Online') }
+      end
+
+      describe 'loading online friends' do
+        before { subject.scroll_down_if_need }
+        it 'must show all friends online' do
+          count_online_friends = subject.find("#friends_summary").text.match(/\d+/)[0].to_i
+          count_visible_online_friends = subject.all('.friends_field>a>b').count
+          expect(count_online_friends).to eq(count_visible_online_friends)
         end
       end
 
-      describe 'Uploading online friends' do
-        it do
-          subject.scroll_down_if_need
-          online_friends = subject.find("#friends_summary").text.match(/\d+/)[0].to_i
-          uploading_online_friends = subject.all('.friends_field>a>b').count
-          expect(online_friends).to eq(uploading_online_friends)
+      describe '#display_online_friends' do
+        it 'should have correct names' do
+          subject.all('.friends_field>a>b').each do |friend|
+            expect(friend.text).to match(/[A-Z]{1}[a-z']+\s{1}[A-Z]{1}[a-z']+/)
+          end
         end
       end
     end
